3abdfa0112a8d5f45b7e3147cd15a67b8bb3653f
Iqbalaulia <16410100012@stikom.edu>
2020-03-06 00:23:21 +0700

    custome font and add image

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -1,12 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Naturally Plus</title>
+
+    <link rel="stylesheet" href="{{ asset('./css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{ asset('./css/custome.css')}}">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,700&display=swap" rel="stylesheet">
+
 </head>
+
 <body>
-    
+
+    <section class="head">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>
+                        The Design Thinking Superpowers
+                    </h1>
+                    <p>
+                        Tools, tutorials, design and innovation experts, all in one place! The most intuitive way to imagine your next user
+                    </p>
+                    <a href="" class="btn btn-primary" role="button">Get Started</a>
+                </div>
+                <div class="col">
+                    <img width="600" src="{{asset('./images/hero-icon.png')}}" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <script type="text/javascript" src="{{asset('./js/bootstrap.js')}}"></script>
 </body>
+
 </html>
